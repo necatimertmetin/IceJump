8eb7f00ab4d5c5423b9cc9655af513d32a9e695f
necatimertmetin <necatimertmetin@gmail.com>
2024-10-25 16:06:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IceJumpGame } from "./IceJumpGame";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <IceJumpGame />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/IceJumpGame.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const IceJumpGame = () => {
+  return <div>IceJumpGame</div>;
+};
